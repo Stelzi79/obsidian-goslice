487e0141ba6d7b22be169f5b04a78d5070846ed5
Stelzi79 <969932+Stelzi79@users.noreply.github.com>
2025-03-11 00:34:22 +0100

    added: gh-action added: dlicer-cli go-inited: gh-action and slicer-cli build: added skeleton for slicer-cli

---
+++ slicer-cli/build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+debug=true
+
+# base_Path="/tmp/slicer-cli"
+base_Path="$(realpath $PWD/..)"
+
+source ../golang.env
+
+if [ $debug = true ]; then
+	echo -e "$BLUE🏷️ base_Path: $YELLOW$base_Path$NC"
+	echo -e "$BLUE🏷️ buildpath: $YELLOW$buildpath$NC"
+	echo -e "$BLUE🏷️ buildpath_slicer: $YELLOW$buildpath_slicer$ND"
+fi
+
+if [ ! -d ${buildpath_slicer} ]; then
+	echo -e "$GREEN📁Creating build directory$NC"
+	mkdir -p ${buildpath_slicer}
+	if [ ! -d ${buildpath_slicer} ]; then
+		echo -e "$RED❌Failed to create build directory: $YELLOW${buildpath_slicer}$NC"
+		return 1
+	fi
+elif [ $debug = true ]; then
+	echo -e "$YELLOW📁Build directory already exists$NC"
+fi
+
+echo -e "$GREEN🚧Clean-Up previous built files$NC"
+rm -rdf ${buildpath_slicer}/*
+
+echo -e "$GREEN🚧Building slicer-cli$NC"
+go build -o ${buildpath_slicer}/$binname_slicer -v ${PWD}
+
+if [ ! -f ${buildpath_slicer}/$binname_slicer ]; then
+	echo -e "\t$RED❌Failed to build slicer-cli$NC"
+	return 1
+else
+	echo -e "\t$GREEN✅slicer-cli built successfully$NC"
+	echo -e "\t$BLUE🏷️Executable: $YELLOW${buildpath_slicer}/$binname_slicer$NC"
+fi
---
+++ slicer-cli/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo -e "🪄Running slicer-cli"
